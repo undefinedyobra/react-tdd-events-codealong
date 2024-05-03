b7acd958e7952dd6cb52212c83e59cc65bdb33db
undefinedyobra <brianmelly008@gmail.com>
2024-05-03 10:02:28 +0300

    Complete Assignment

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,6 +1,36 @@
+
 import { render, screen } from "@testing-library/react";
 import App from "../App";
 
+import userEvent from "@testing-library/user-event";
 import '@testing-library/jest-dom';
 
 // Code tests here
+test("Pizza checkbox is initially unchecked", () => {
+    render(<App />)
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i })
+    expect(addPepperoni).not.toBeChecked()
+})
+
+test("Toppings list initially contains only cheese", () => {
+    render(<App />)
+    expect(screen.getAllByRole("listitem").length).toBe(1);
+    expect(screen.getByText("Cheese")).toBeInTheDocument();
+    expect(screen.queryByText("Pepperoni")).not.toBeInTheDocument()
+})
+
+test("Checkboxes become checked when user clicks them", () => {
+    render(<App />)
+    const addPepperoni = screen.getByRole("checkbox", { name: /add pepperoni/i })
+    userEvent.click(addPepperoni)
+    expect(addPepperoni).toBeChecked();
+})
+
+test("Toppings appear in toppings list when checked", () => {
+    render(<App />)
+    const addPepperoni = screen.getByRole("checkbox", {name: /add pepperoni/i})
+    userEvent.click(addPepperoni)
+    expect(screen.getAllByRole("listitem").length).toBe(2);
+    expect(screen.getByText("Cheese")).toBeInTheDocument();
+    expect(screen.getByText("Pepperoni")).toBeInTheDocument();
+})
